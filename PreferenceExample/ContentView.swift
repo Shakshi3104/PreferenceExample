21a98557a4bdb3bd848a64d01b242c5e05ccdabe
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2021-10-07 13:33:37 +0900

    Initial Commit

---
+++ PreferenceExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PreferenceExample
+//
+//  Created by MacBook Pro M1 on 2021/10/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PreferenceExample/PreferenceExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreferenceExampleApp.swift
+//  PreferenceExample
+//
+//  Created by MacBook Pro M1 on 2021/10/07.
+//
+
+import SwiftUI
+
+@main
+struct PreferenceExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
